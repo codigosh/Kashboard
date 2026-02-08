d6fd59b5d6cdccc904070c84de59743581ff0e75
K4wit0 <github@codigosh.com>
2026-02-08 00:36:28 +0000

    chore: bump version to 1.1.4-beta.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,5 +3,5 @@ package version
 var (
 	// Current is the current version of the application.
 	// This usually gets overwritten by build flags (ldflags).
-	Current = "v1.1.4-beta.1"
+	Current = "v1.1.4-beta.2"
 )
