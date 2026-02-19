c714bd8680074fbc07caeeec2ef83ab160551d5b
K4wit0 <github@codigosh.com>
2026-02-19 03:16:33 +0000

    feat: v1.3.0 release: border customization, public demo, and unified feedback

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,5 +3,5 @@ package version
 var (
 	// Current is the current version of the application.
 	// This usually gets overwritten by build flags (ldflags).
-	Current = "v1.3.0-Beta.25"
+	Current = "v1.3.0"
 )
